58b4ff6fe2c9d7486fdffe5627144b9372665ba8
SKY POINT 9 <SKY POINT 9@192.168.1.103>
2016-02-24 14:18:10 +0530

    Soumya added

--- LotoPlay/src/jewel/Jewel.java
+++ LotoPlay/src/jewel/Jewel.java
@@ -5,6 +5,7 @@ public class Jewel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Jewel Saha");
+		System.out.println("Soumya Paul");
 	}
 
 }
