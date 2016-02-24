ce9b01073fbfbb09ea23a076d23ff6577ead3ab4
JeWel <JeWel@192.168.1.116>
2016-02-24 18:03:54 +0530

    eee

---
+++ LotoPlay/src/jewel/Jewel.java
@@ -0,0 +1,18 @@
+package jewel;
+
+public class Jewel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jewel Saha");
+		System.out.println("Skypoint33");
+		
+		System.out.println("Jai_SRI_RAM");
+		System.out.println("Skypoint3");
+		
+		System.out.println("Jewel 332");
+		
+		
+	}
+
+}
