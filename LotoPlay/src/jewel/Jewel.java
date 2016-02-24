5570010719a4197f8c37afe2deb6393c598ff9d3
SKY POINT 9 <SKY POINT 9@192.168.1.103>
2016-02-24 15:27:18 +0530

    hhhhh

--- LotoPlay/src/jewel/Jewel.java
+++ LotoPlay/src/jewel/Jewel.java
@@ -5,7 +5,6 @@ public class Jewel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Jewel Saha");
-		System.out.println("Soumya Paul");
 		System.out.println("Skypoint");
 		System.out.println("Skypoint2");
 		
