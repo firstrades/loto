856ef31b7d78abae4a313fc43752da8fe5c09b20
JeWel <JeWel@192.168.1.116>
2016-02-24 16:26:08 +0530

    Jewel 221

--- LotoPlay/src/jewel/Jewel.java
+++ LotoPlay/src/jewel/Jewel.java
@@ -10,7 +10,7 @@ public class Jewel {
 		System.out.println("Jai_SRI_RAM");
 		System.out.println("Skypoint3");
 		
-		System.out.println("First Branch");
+		System.out.println("Jewel 221");
 		
 		
 	}
