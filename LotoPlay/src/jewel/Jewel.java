11ae0ea6a282c5e6726a163e3aa2c5d8a1d6eaa9
JeWel <JeWel@192.168.1.116>
2016-02-25 11:09:19 +0530

    Branch2

--- LotoPlay/src/jewel/Jewel.java
+++ LotoPlay/src/jewel/Jewel.java
@@ -7,8 +7,7 @@ public class Jewel {
 		System.out.println("Jewel Saha");
 		System.out.println("Skypoint33");
 		
-		System.out.println("Jai_SRI_RAM");
-		System.out.println("Skypoint3");
+		
 		
 		System.out.println("Changed in Jewel");
 		System.out.println("JEWEL Da is Great");
